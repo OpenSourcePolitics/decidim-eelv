fbf2f3d373606d6b0d7e6c5f775152c8b1a10a7d
MoretS <s.moret@ecole-ipssi.net>
2021-03-22 14:45:37 +0100

    Add app to Newrelic + add Newrelic cookie to Orejime banner

--- config/initializers/cookies.rb
+++ config/initializers/cookies.rb
@@ -6,5 +6,11 @@ Rails.application.config.cookies = [
     title: "Matomo",
     cookies: %w(matomo_session pk_id pk_ses _pk_ref _pk_cvar),
     purposes: %w(tracking analytics)
+  },
+  {
+    name: "jsessionid",
+    title: "JSESSIONID",
+    cookies: %w(JSESSIONID),
+    purposes: %w(session tracking analytics)
   }
 ]
