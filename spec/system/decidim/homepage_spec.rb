87f0ed6e175f4c6ce1e8a195d9e334e691602634
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-10-06 15:27:15 +0200

    Update homepage_spec.rb

--- spec/system/decidim/homepage_spec.rb
+++ spec/system/decidim/homepage_spec.rb
@@ -432,8 +432,6 @@ describe "Homepage", type: :system do
             expect(zipfile.glob("*open-data-proposals.csv").length).to eq(1)
             expect(zipfile.glob("*open-data-results.csv").length).to eq(1)
             expect(zipfile.glob("*open-data-meetings.csv").length).to eq(1)
-            expect(zipfile.glob("*open-data-elections.csv").length).to eq(1)
-            expect(zipfile.glob("*open-data-votings.csv").length).to eq(1)
           end
         end
       end
